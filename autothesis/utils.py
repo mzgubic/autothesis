e64f759cdcaa1a02bad873ed65d8ed04990882ad
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2019-11-14 14:52:33 +0000

    add model directory

--- autothesis/utils.py
+++ autothesis/utils.py
@@ -4,4 +4,6 @@ from pathlib import Path
 data_path = Path(os.getenv('DATA'))
 src_path = Path(os.getenv('SRC'))
 
+def model_dir_name(cls_name, token, max_len, n_steps):
 
+    return data_path / 'run' / token / '{}_{}_{}steps'.format(cls_name, max_len, n_steps)
--- scripts/train.py
+++ scripts/train.py
@@ -1,8 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import os
+import pickle
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
-
 import generate
+import utils
 from model import CharacterRNN
 
 parser = argparse.ArgumentParser()
@@ -10,6 +15,7 @@ parser.add_argument('--token', default='character', choices=['character', 'word'
 parser.add_argument('--max-len', type=int, default=20)
 parser.add_argument('--small', action='store_true')
 parser.add_argument('--n-steps', type=int, default=1000)
+parser.add_argument('--force', action='store_true')
 args = parser.parse_args()
 
 if __name__ == '__main__':
@@ -18,19 +24,26 @@ if __name__ == '__main__':
     temperature = 0.5
     how_many = 50
 
-    vocab = generate.get_vocab(args.token, small=small)
+    vocab = generate.get_vocab(args.token, small=args.small)
 
     # build the model
     model = CharacterRNN(args.token, vocab)
+
+    # directory housekeeping
+    model_dir = utils.model_dir_name(type(model).__name__, args.token, args.max_len, args.n_steps)
+    if os.path.exists(model_dir) and args.force:
+        os.system('rm -r {}/*'.format(model_dir))
+    else:
+        os.makedirs(model_dir)
 
     # create criterion and optimiser
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
     # training loop
-    every_n = int(total_n/100)
+    every_n = int(args.n_steps/100)
     running_loss = 0
-    losses = []
+    training_losses = []
     for i, (batch, labels) in enumerate(generate.generate('train', token=args.token, max_len=args.max_len, small=args.small)):
 
         # one hot encode
@@ -52,20 +65,32 @@ if __name__ == '__main__':
         # monitor the losses
         running_loss += loss
         if i % every_n == (every_n-1):
-            print('{}/{} done'.format(i+1, total_n))
-            losses.append(running_loss/every_n)
+            training_losses.append(running_loss/every_n)
             running_loss = 0
-            print(model.compose('The Standard Model of pa', temperature, how_many))
+
+            # monitor progress
+            monitor = ['\n{}/{} done'.format(i+1, args.n_steps)]
+            monitor.append(model.compose('The Standard Model of pa', temperature, how_many))
+            monitor.append(model.compose('[23] Aad G', temperature, how_many))
+            monitor.append(model.compose('arxiv.', temperature, how_many))
+            for m in monitor:
+                print(m)
+                os.system('echo "{}" >> {}'.format(m, model_dir/'out_stream.txt'))
+
 
         if i >= args.n_steps:
             break
     
-    fig, ax = plt.subplots()
-    ax.plot(np.arange(len(losses))*every_n, losses, label='training loss')
-    ax.set_xlabel('training step')
-    ax.set_ylabel('loss')
-    ax.set_ylim(0, ax.get_ylim()[1])
-    ax.legend()
-    plt.savefig('Losses.pdf')
+    # save the losses
+    pickle.dump({'training_losses':training_losses}, open(model_dir/ 'losses.pkl', 'wb'))
+    
+
+    #fig, ax = plt.subplots()
+    #ax.plot(np.arange(len(losses))*every_n, losses, label='training loss')
+    #ax.set_xlabel('training step')
+    #ax.set_ylabel('loss')
+    #ax.set_ylim(0, ax.get_ylim()[1])
+    #ax.legend()
+    #plt.savefig('Losses.pdf')
 
 
