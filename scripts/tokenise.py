8f215dc26afbc7c488ab49eb0cb5545f6ba9c90b
Miha Zgubic <miha.zgubic@physics.ox.ac.uk>
2020-02-23 00:18:39 +0000

    improve phrasify performance

--- scripts/tokenise.py
+++ scripts/tokenise.py
@@ -7,12 +7,8 @@ import h5py
 import numpy as np
 import pandas as pd
 import pickle
-import nltk
-from nltk.corpus import words
 from generate import Vocab
 import utils
-from julia import Main
-Main.include("../autothesis/fast.jl")
 
 
 def sweep_lines(fpath):
@@ -66,7 +62,7 @@ def index(tokens):
         # add to index array
         idx_list.append(token2idx[token])
 
-        if i>=1000000 and args.small:
+        if i>=100000 and args.small:
             break
 
     return token2idx, token_counts, np.array(idx_list)
@@ -78,104 +74,69 @@ def phrasify(token2idx, token_counts, idx_array):
     Build phrases based on the score:
             score = (count(ti, tj) - delta) / (count(ti) * count(tj))
 
-    Expand the vocabulary by 1%, for the phrases with the highest score.
+    Expand the vocabulary by 1% using the phrases with the highest score.
     """
-    # build the coocurrency matrix
-    n_tokens = len(token_counts)
-    counts_bi = np.zeros(shape=(n_tokens, n_tokens))
-    counts_first = np.zeros(shape=(n_tokens, n_tokens))
-    counts_second = np.zeros(shape=(n_tokens, n_tokens))
-    
-    # loop over the indices to count bigrams
-    for k in range(len(idx_array)-1):
-        idx_i, idx_j = idx_array[k], idx_array[k+1]
-        counts_bi[idx_i, idx_j] += 1
-        counts_first[idx_i,:] += 1
-        counts_second[:, idx_j] += 1
-    # and take care of first and last token
-    counts_first[idx_array[-1], :] += 1
-    counts_second[:, idx_array[0]] += 1
 
-    # compute the score
-    delta = np.max(counts_bi) / 100.
-    scores = (counts_bi - delta) / (counts_first * counts_second)
-    scores = np.nan_to_num(scores, copy=False, nan=0, posinf=0, neginf=0)
+    # count phrase occurence
+    bigram_counts = {t:{} for t in token2idx}
+    i2t = {token2idx[t]:t for t in token2idx}
+    n_tokens = len(idx_array)
+    for i in range(n_tokens-1):
+        this_token = i2t[idx_array[i]]
+        next_token = i2t[idx_array[i+1]]
+        try:
+            bigram_counts[this_token][next_token] += 1
+        except KeyError:
+            bigram_counts[this_token][next_token] = 1
 
-    # increase vocabulary by 1%
-    i2t = {token2idx[k]:k for k in token2idx}
-    threshold = np.min(sorted(scores.flatten(), reverse=True)[:int(1+0.01*n_tokens)])
-    idx_pairs = np.argwhere(scores >= threshold)
-    
-    # add the phrases to token2idx
-    for idx_i, idx_j in idx_pairs:
-        phrase = '{} {}'.format(i2t[idx_i], i2t[idx_j])
+    # compute the scores
+    counts = [bigram_counts[first][second] for first in bigram_counts for second in bigram_counts[first]]
+    delta = np.max(counts) / 100.
+    bigram_scores = {ft:{st:0 for st in bigram_counts[ft]} for ft in bigram_counts}
+    for first in bigram_counts:
+        for second in bigram_counts[first]:
+            score = (bigram_counts[first][second] - delta) / (token_counts[first] * token_counts[second])**0.5
+            bigram_scores[first][second] = score
+
+    # keep good phrases (above threshold ones)
+    # (increase vocabulary by 1%)
+    scores = [bigram_scores[first][second] for first in bigram_scores for second in bigram_scores[first]]
+    threshold = np.min(sorted(scores, reverse=True)[:int(1+0.01*n_tokens)])
+    good_phrases = {(first, second) for first in bigram_scores for second in bigram_scores[first] if bigram_scores[first][second] > threshold}
+
+    # add good phrases to token2idx
+    for first, second in good_phrases:
+        phrase = '{} {}'.format(first, second)
         token2idx[phrase] = len(token2idx)
 
-    # update the idx_array and the token counts
-    new_idx_array = []
-    token_counts = {t:0 for t in token2idx}
-    itr = iter(range(len(idx_array) - 1))
+    # update idx_array and token_counts
+    itr = iter(range(n_tokens - 1))
+    new_idx_list = []
+    token_counts = {token:0 for token in token2idx}
     for i in itr:
 
-        # current and next index
-        this_idx = idx_array[i]
-        next_idx = idx_array[i+1]
-        phrase = '{} {}'.format(i2t[this_idx], i2t[next_idx])
+        # is the token combination a phrase?
+        this_token = i2t[idx_array[i]]
+        next_token = i2t[idx_array[i+1]]
+        phrase = '{} {}'.format(this_token, next_token)
 
-        # if they are not a phrase
-        if phrase not in token2idx:
-            token = i2t[this_idx]
-            new_idx_array.append(this_idx) # add index to the new list
-            token_counts[token] += 1 # and count
-
-        # if they are a phrase
-        else:
-            phrase = '{} {}'.format(i2t[this_idx], i2t[next_idx])
-            new_idx_array.append(token2idx[phrase]) # add to new list
-            token_counts[phrase] += 1 # and add a count
+        # if yes, record the phrase and skip the second token of the phrase
+        if phrase in token2idx:
+            token = phrase
+            # skip the second token of the phrase
             try:
-                skip = next(itr) # skip the second part of the phrase
+                _ = next(itr)
             except StopIteration:
-                continue
+                pass
+        # if not, just use the token
+        else:
+            token = this_token
+        
+        # update idx array
+        new_idx_list.append(token2idx[token])
+        token_counts[token] += 1
 
-    return token2idx, token_counts, np.array(new_idx_array)
-
-
-@utils.timeit
-def reduce_tokens(token2idx, token_counts, idx_array, args):
-
-    # TODO: delete these two lines
-    token2idx = token2idx.copy()
-    token_counts = token_counts.copy()
-
-    show_first(20, token2idx, idx_array)
-
-    new_t2i = {'<unk>':0}
-    new_idx_array = np.copy(idx_array)
-    for token in sorted(token2idx.keys()):
-
-        # this is the new index
-        old_idx = token2idx[token]
-        count = token_counts[token]
-
-        # new index: merged with unknown if below threshold
-        above_threshold = count >= args.threshold
-        new_idx = len(set(new_t2i.values())) if above_threshold else 0
-
-        # change the t2i and idx_array
-        new_t2i[token] = new_idx
-        there = np.where(idx_array == old_idx)
-        new_idx_array[there] = new_idx
-
-        # remove counts
-        if not above_threshold:
-            if token != '<unk>':
-                token_counts[token] = 0
-            token_counts['<unk>'] += count
-
-    show_first(20, new_t2i, new_idx_array)
-
-    return new_t2i, token_counts, new_idx_array
+    return token2idx, token_counts, np.array(new_idx_list)
 
 
 def split_token_array(idx_array, args):
@@ -228,8 +189,8 @@ if __name__ == '__main__':
     parser.add_argument('--val-frac', type=float, default=0.1)
     parser.add_argument('--test-frac', type=float, default=0.1)
     parser.add_argument('--small', action='store_true')
-    parser.add_argument('--threshold', type=int, default=300, help='Minimum frequency of tokens to be kept (otherwise: <unk>)')
-    parser.add_argument('--phrase-length', type=int, default=3, help='Max phrase length')
+    #parser.add_argument('--threshold', type=int, default=300, help='Minimum frequency of tokens to be kept (otherwise: <unk>)') # reducing tokens handled differently
+    parser.add_argument('--phrase-runs', type=int, default=2, help='Max phrase length')
     args = parser.parse_args()
 
     # some helpers
@@ -242,46 +203,22 @@ if __name__ == '__main__':
     tokens = sweep_tokens(lines)
     token2idx, token_counts, idx_array = index(tokens)
 
-    show_first(20, token2idx, idx_array)
-
-    # for words: add phrases and reduce number of tokens 
+    # for words: add phrases
     if args.token == 'word':
 
-        # throw away tokens which dont appear often enough
-        print('Throwing away rare tokens')
-        #token2idx, token_counts, idx_array = reduce_tokens(token2idx, token_counts, idx_array, args)
-        t0 = time.time()
-        p_t2i, p_tc, p_ia = reduce_tokens(token2idx, token_counts, idx_array, args)
-        print('python took {}'.format(time.time() - t0))
+        # identify phrases and merge the individual word tokens into phrases
+        print('Identifying phrases')
+        for _ in range(args.phrase_runs):
+            token2idx, token_counts, idx_array = phrasify(token2idx, token_counts, idx_array)
 
-        t0 = time.time()
-        j_t2i, j_tc, j_ia = Main.reduce_tokens(token2idx, token_counts, idx_array, args.threshold)
-        print('julia took {}'.format(time.time() - t0))
-
-        for i, token in enumerate(token2idx):
-            print()
-            print(token)
-            print('o', token2idx[token], token_counts[token])
-            print('p', p_t2i[token], p_tc[token])
-            print('j', j_t2i[token], j_tc[token])
-            if i == 5:
-                break
-
-        ## identify phrases and merge the individual word tokens into phrases
-        #print('length of t2i is {}, idx_array {}'.format(len(token2idx), type(idx_array)))
-        #print('Identifying phrases')
-        #for _ in range(args.phrase_length - 1):
-        #    token2idx, token_counts, idx_array = phrasify(token2idx, token_counts, idx_array)
-        #    print('length of t2i is {}, idx_array {}'.format(len(token2idx), type(idx_array)))
-
-    # second sweep to fill the output arrays
+    # split arrays
     print('Split the tokens into training, validation, and test sets')
     train, val, test = split_token_array(idx_array, args)
 
     # save the array
     save_tokens(train, val, test, token2idx, args)
 
-    for length in range(args.phrase_length - 1):
+    for length in range(args.phrase_runs):
         print()
         phrases = [p for p in token2idx if p.count(' ') > length]
         for p in phrases:
